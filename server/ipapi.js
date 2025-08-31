51924195b722f88580a010d392ce486ed05dd794
Omri Yahav <122606148+OmriYahav@users.noreply.github.com>
2025-08-31 13:58:03 +0300

    Format IP lookup to expected fields

--- server/ipapi.js
+++ server/ipapi.js
@@ -11,7 +11,58 @@ module.exports = function ipapi(ip) {
       res.on('data', chunk => (data += chunk));
       res.on('end', () => {
         try {
-          resolve(JSON.parse(data));
+          const json = JSON.parse(data);
+          if (json && json.error) {
+            return reject(new Error(json.reason || 'ipapi error'));
+          }
+          const {
+            ip,
+            city,
+            region,
+            region_code,
+            country_code,
+            country_code_iso3,
+            country_name,
+            country_capital,
+            country_tld,
+            continent_code,
+            in_eu,
+            postal,
+            latitude,
+            longitude,
+            timezone,
+            utc_offset,
+            country_calling_code,
+            currency,
+            currency_name,
+            languages,
+            asn,
+            org,
+          } = json;
+          resolve({
+            ip,
+            city,
+            region,
+            region_code,
+            country_code,
+            country_code_iso3,
+            country_name,
+            country_capital,
+            country_tld,
+            continent_code,
+            in_eu,
+            postal,
+            latitude,
+            longitude,
+            timezone,
+            utc_offset,
+            country_calling_code,
+            currency,
+            currency_name,
+            languages,
+            asn,
+            org,
+          });
         } catch (err) {
           reject(err);
         }
