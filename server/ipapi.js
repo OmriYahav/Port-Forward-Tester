fa5db037e6756435909633cc6a4f4ee9a0c7eabe
Omri Yahav <122606148+OmriYahav@users.noreply.github.com>
2025-08-31 13:44:06 +0300

    Handle IP API errors with fallback

--- server/ipapi.js
+++ server/ipapi.js
@@ -3,6 +3,10 @@ const https = require('https');
 module.exports = function ipapi(ip) {
   return new Promise((resolve, reject) => {
     const req = https.get(`https://ipapi.co/${ip}/json/`, res => {
+      if (res.statusCode !== 200) {
+        res.resume();
+        return reject(new Error(`ipapi status ${res.statusCode}`));
+      }
       let data = '';
       res.on('data', chunk => (data += chunk));
       res.on('end', () => {
